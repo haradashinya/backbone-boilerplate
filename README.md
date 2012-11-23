e1a9374decdbffad0fa4a8d1495ba59d49941972
okamurayasuyuki <haradashinya@gmail.com>
2012-11-24 00:48:06 +0900

    fixed css

---
+++ README.md
@@ -0,0 +1,12 @@
+# This is my own backbone-boilerplate.
+
+
+This boilerplate's use technology following this
+
+* sinatra
+* backbone
+* asset-pipeline
+* sass
+* js
+* Handlebars
+* loader 
--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,10 +1,8 @@
-//= require zepto.min
-//= require handlebars-1.0.rc.1
-//= json2
+//= require lib/zepto.min
+//= require lib/handlebars-1.0.rc.1
 //= require lib/underscore
 //= require lib/backbone
 //= require lib/orig_template/loader
 //= require_tree .
-//= require foo
 //= require router
 

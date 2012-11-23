085b45938286aa787866bb61bd47943724001171
okamurayasuyuki <haradashinya@gmail.com>
2012-11-23 19:32:05 +0900

    add handlebar.vim

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require zepto.min
+//= require handlebars-1.0.rc.1
 //= json2
 //= require underscore-min
 //= require backbone-min
+//= require_tree .
 //= require_tree ../templates
-//= require foo
-//= require a
